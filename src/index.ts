22b95f6e039417ceeed52fd40a1dd71f5641bc51
Tale152 <alessandro.talmi@studio.unibo.it>
2022-06-16 11:03:14 +0200

    added cors and bodyParser

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import bodyParser from "body-parser"
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT;
+const app: Express = express()
+const port = process.env.PORT
+
+app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', (req: Request, res: Response) => {
   console.log(req.body)
-  res.send('Express + TypeScript Server');
-});
+  res.send('Express + TypeScript Server')
+})
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+})
