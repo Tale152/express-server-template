390e3b1a47d8abb3df3638f689802f81cbdb1836
Tale152 <alessandro.talmi@studio.unibo.it>
2022-06-18 01:40:59 +0200

    implemented JwtTokenGenerator

---
+++ src/core/frameworks_and_drivers/JwtTokenGenerator.ts
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+import TokenGenerator from "../interface_adapters/TokenGenerator"
+
+export default class JwtTokenGenerator implements TokenGenerator{
+
+    private constructor(){}
+
+    static createInstance(){
+        return new JwtTokenGenerator()
+    }
+
+    generate(str: string): string{
+        //token presence gets checked when launching server so it's impossible for it to be undefined but the check has to be performed to make Typescript happy
+        return jwt.sign(str, process.env.TOKEN_SECRET !== undefined ? process.env.TOKEN_SECRET : "")
+    }
+    
+}
