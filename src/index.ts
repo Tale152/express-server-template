d147098c638eeab2fbeb3546f37414386f775b01
Tale152 <alessandro.talmi@studio.unibo.it>
2022-06-19 12:21:57 +0200

    moved index into src

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose"
-import server from "./src/server"
+import server from "./server"
 
 dotenv.config()
 if(process.env.DB_ADDRESS !== undefined){
     mongoose
     .connect(process.env.DB_ADDRESS)
     .then(async () => {
+        console.log("Connection to DB succesful")
         server.listen(process.env.PORT, () => {
             console.log("Server is listening on port " + process.env.PORT)
         })
