47c410a1aa667a05924f8f18378e99b12dae49db
Tale152 <alessandro.talmi@studio.unibo.it>
2022-07-07 17:48:47 +0200

    fixed style in EnvVariablesSingleton

--- src/setup/EnvVariablesSingleton.ts
+++ src/setup/EnvVariablesSingleton.ts
@@ -1,5 +1,10 @@
 import dotenv from 'dotenv';
 
+/**
+ * Singleton used to access environment variables in a type-safe way,
+ * throwing error if one or more environment variables are not defined
+ * or defined incorrectly.
+ */
 export default class EnvVariablesSingleton {
   private static inst: EnvVariablesSingleton | undefined = undefined;
   private envPort: number;
@@ -8,36 +13,70 @@ export default class EnvVariablesSingleton {
   private secret: string;
   private validity: string;
 
+  /**
+   * Private constructor, only exposing the static instance getter following
+   * singleton pattern. Here the variables are initialized using private
+   * handlers specific for the expected type of variable.
+   * @throws if one or more environment variables are not defined
+   * or defined incorrectly.
+   */
   private constructor() {
     dotenv.config();
-
-    if (process.env.PORT !== undefined && !isNaN(parseInt(process.env.PORT))) {
-      this.envPort = parseInt(process.env.PORT);
-    } else {
-      throw Error('There is something wrong with the environment variable PORT: ' + process.env.PORT);
-    }
-    if (process.env.DB_ADDRESS !== undefined && typeof process.env.DB_ADDRESS === 'string') {
-      this.db = process.env.DB_ADDRESS;
-    } else {
-      throw Error('There is something wrong with the environment variable DB_ADDRESS: ' + process.env.DB_ADDRESS);
-    }
-    if (process.env.ENCRYPTION_SALT !== undefined && !isNaN(parseInt(process.env.ENCRYPTION_SALT))) {
-      this.salt = parseInt(process.env.ENCRYPTION_SALT);
-    } else {
-      throw Error('There is something wrong with the environment variable ENCRYPTION_SALT: ' + process.env.ENCRYPTION_SALT);
-    }
-    if (process.env.TOKEN_SECRET !== undefined && typeof process.env.TOKEN_SECRET === 'string') {
-      this.secret = process.env.TOKEN_SECRET;
-    } else {
-      throw Error('There is something wrong with the environment variable TOKEN_SECRET: ' + process.env.TOKEN_SECRET);
-    }
-    if (process.env.TOKEN_VALIDITY !== undefined && typeof process.env.TOKEN_VALIDITY === 'string') {
-      this.validity = process.env.TOKEN_VALIDITY;
-    } else {
-      throw Error('There is something wrong with the environment variable TOKEN_VALIDITY: ' + process.env.TOKEN_VALIDITY);
-    }
+    this.envPort = this.getNum(process.env.PORT, 'PORT');
+    this.db = this.getStr(process.env.DB_ADDRESS, 'DB_ADDRESS');
+    this.salt = this.getNum(process.env.SALT, 'ENCRYPTION_SALT');
+    this.secret = this.getStr(process.env.TOKEN_SECRET, 'TOKEN_SECRET');
+    this.validity = this.getStr(process.env.TOKEN_VALIDITY, 'TOKEN_VALIDITY');
   }
 
+  /**
+   * Executes checks on the specified string environment variable.
+   * @param {string | undefined} v the value of the environment variable
+   * @param {string} name the name to display in the error message should
+   * the variable be not defined or defined incorrectly
+   * @return {string} the type-safe string environment variable
+   * @throws if v is undefined, not a string or an empty string
+   */
+  private getStr(v: string | undefined, name: string): string {
+    if (v !== undefined && typeof v === 'string' && v.trim() !== '') {
+      return v.trim();
+    }
+    throw Error(this.errorString(v, name));
+  }
+
+  /**
+   * Executes checks on the specified numeric environment variable.
+   * @param {string | undefined} v the value of the environment variable
+   * @param {string} name the name to display in the error message should
+   * the variable be not defined or defined incorrectly
+   * @return {number} the type-safe numeric environment variable
+   * @throws if v is undefined or not a number
+   */
+  private getNum(v: string | undefined, name: string): number {
+    if (v !== undefined && !isNaN(parseInt(v))) {
+      return parseInt(v);
+    }
+    throw Error(this.errorString(v, name));
+  }
+
+  /**
+   * String that will be displayed in the error message should
+   * the variable be not defined or defined incorrectly.
+   * @param {string | undefined} v the value of the environment variable
+   * @param {string} name the name of the variable
+   * @return {string} the string created using the provided arguments
+   */
+  private errorString(v: string | undefined, name: string): string {
+    return 'Something is wrong with the environment variable' + name + ': ' + v;
+  }
+
+  /**
+   * Static getter used to access the instance following
+   * the singleton pattern.
+   * @return {EnvVariablesSingleton} the singleton instance
+   * @throws if one or more environment variables are not defined
+   * or defined incorrectly.
+   */
   static get instance(): EnvVariablesSingleton {
     if (EnvVariablesSingleton.inst === undefined) {
       EnvVariablesSingleton.inst = new EnvVariablesSingleton();
@@ -45,22 +84,40 @@ export default class EnvVariablesSingleton {
     return EnvVariablesSingleton.inst;
   }
 
+  /**
+   * @return {number} the type-safe value of the PORT environment variable
+   */
   get port(): number {
     return this.envPort;
   }
 
+  /**
+   * @return {string} the type-safe value of the DB_ADDRESS environment variable
+   */
   get dbAddress(): string {
     return this.db;
   }
 
+  /**
+   * @return {number} the type-safe value of the ENCRYPTION_SALT
+   * environment variable
+   */
   get encryptionSalt(): number {
     return this.salt;
   }
 
+  /**
+   * @return {string} the type-safe value of the TOKEN_SECRET
+   * environment variable
+   */
   get tokenSecret(): string {
     return this.secret;
   }
 
+  /**
+   * @return {string} the type-safe value of the TOKEN_VALIDITY
+   * environment variable
+   */
   get tokenValidity(): string {
     return this.validity;
   }
