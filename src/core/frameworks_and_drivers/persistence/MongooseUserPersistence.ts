3060ed7aa9edef2d5124ab7a158d1937a8ec2ce7
Tale152 <alessandro.talmi@studio.unibo.it>
2022-07-07 19:23:54 +0200

    changed lint on commit workflow label

--- src/core/frameworks_and_drivers/persistence/MongooseUserPersistence.ts
+++ src/core/frameworks_and_drivers/persistence/MongooseUserPersistence.ts
@@ -10,9 +10,7 @@ export default class MongooseUserPersistence implements UserPersistence {
   }
 
   exists(username: string): Promise<boolean> {
-    return new Promise((resolve) => {
-      this.getByUsername(username).then((user) => resolve(user !== undefined));
-    });
+    return new Promise((resolve) => {this.getByUsername(username).then((user) => resolve(user !== undefined));});
   }
 
   createNew(user: User): Promise<boolean> {
