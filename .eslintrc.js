a4a1bc22aec3196ffaf33923cc33a5afe17d63e4
Tale152 <alessandro.talmi@studio.unibo.it>
2022-07-07 18:53:07 +0200

    fixed line length error for long imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
         "@typescript-eslint/semi": ["error"],
         "indent": "off",
         "@typescript-eslint/indent": ["error", 2],
+        'max-len': [2, { code: 80, ignorePattern: '^import .*' }]
     }
 }
