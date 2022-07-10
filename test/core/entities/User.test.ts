91d69f083e7726f6b41b6483f368810ae285e19b
Tale152 <Tale152@users.noreply.github.com>
2022-07-10 11:18:47 +0000

    Apply automatic lint changes

--- test/core/entities/User.test.ts
+++ test/core/entities/User.test.ts
@@ -1,9 +1,9 @@
 import User from '../../../src/core/entities/User';
 
-const emptyString: string = "";
-const validUsername: string = "user";
-const validPassword: string = "password";
-const extraSpaces: string = "\t  \t";
+const emptyString: string = '';
+const validUsername: string = 'user';
+const validPassword: string = 'password';
+const extraSpaces: string = '\t  \t';
 
 test('An User must have a valid username', () => {
   // @ts-ignore
@@ -11,7 +11,7 @@ test('An User must have a valid username', () => {
   // @ts-ignore
   assertUserThrowsOnNewInstance(null, validPassword);
   assertUserThrowsOnNewInstance(emptyString, validPassword);
-})
+});
 
 test('An User must have a valid password', () => {
   // @ts-ignore
@@ -19,24 +19,30 @@ test('An User must have a valid password', () => {
   // @ts-ignore
   assertUserThrowsOnNewInstance(validUsername, null);
   assertUserThrowsOnNewInstance(validUsername, emptyString);
-})
+});
 
 test('An User should return the correct username and password', () => {
   const user: User = User.createInstance(validUsername, validPassword);
   expect(user.username).toEqual(validUsername);
   expect(user.password).toEqual(validPassword);
-})
+});
 
 test('Username and password should not contain extra spaces', () => {
-  const user: User = User.createInstance(addExtraSpaces(validUsername), addExtraSpaces(validPassword));
+  const user: User = User.createInstance(
+    addExtraSpaces(validUsername),
+    addExtraSpaces(validPassword),
+  );
   expect(user.username).toEqual(validUsername);
   expect(user.password).toEqual(validPassword);
-})
+});
 
-function assertUserThrowsOnNewInstance(username: string, password: string): void{
+function assertUserThrowsOnNewInstance(
+  username: string,
+  password: string,
+): void {
   expect(() => User.createInstance(username, password)).toThrow();
 }
 
-function addExtraSpaces(str: string): string{
+function addExtraSpaces(str: string): string {
   return extraSpaces + str + extraSpaces;
 }
