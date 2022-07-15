34fd1024e63f6af4bd497becace0dc9f9e04d619
Tale152 <alessandro.talmi@studio.unibo.it>
2022-07-15 11:31:14 +0200

    tested BcryptEncryptionHandler

---
+++ test/core/frameworks_and_drivers/BcryptEncryptionHandler.test.ts
@@ -0,0 +1,15 @@
+import * as bcrypt from 'bcrypt';
+import BcryptEncryptionHandler from '../../../src/core/frameworks_and_drivers/security/BcryptEncryptionHandler';
+
+test('The BcryptEncryptionHandler encrypt and compare functions should behave like the bcrypt ones', async () => {
+  const encryptionHandler = BcryptEncryptionHandler.createInstance();
+  const toEncrypt = 'a string with trailing space ';
+  const encrypted = await encryptionHandler.encrypt(toEncrypt);
+  expect(await bcrypt.compare(toEncrypt, encrypted)).toEqual(
+    await encryptionHandler.compare(toEncrypt, encrypted),
+  );
+  const differentString = toEncrypt + 'abc';
+  expect(await bcrypt.compare(differentString, encrypted)).toEqual(
+    await encryptionHandler.compare(differentString, encrypted),
+  );
+});
