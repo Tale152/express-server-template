d6dc5b963a0e74c22d5ada57e9cdaac16888d92a
Tale152 <alessandro.talmi@studio.unibo.it>
2022-07-18 16:15:16 +0200

    commented JwtTokenGenerator

--- src/core/frameworks_and_drivers/security/JwtTokenGenerator.ts
+++ src/core/frameworks_and_drivers/security/JwtTokenGenerator.ts
@@ -4,15 +4,19 @@ import {DecryptedToken, EncryptedToken} from '../../entities/Token';
 import TokenGenerator from '../../interface_adapters/security/TokenGenerator';
 import {isStringEmpty} from '../../utils/checks/stringChecks';
 
+/**
+* Concrete implementation of the TokenGenerator interface using
+* the jsonwebtoken library.
+*/
 export default class JwtTokenGenerator implements TokenGenerator {
-  private secret: string;
-  private validity: string;
+  private secret: string = EnvVariablesSingleton.instance.tokenSecret;
+  private validity: string = EnvVariablesSingleton.instance.tokenValidity;
 
-  constructor() {
-    this.secret = EnvVariablesSingleton.instance.tokenSecret;
-    this.validity = EnvVariablesSingleton.instance.tokenValidity;
-  }
-
+  /**
+   * Encrypts a DecryptedToken using the sign function of jwt.
+   * @param {DecryptedToken} token a DecryptedToken to encrypt
+   * @return {EncryptedToken} the newly encrypted token
+   */
   encrypt(token: DecryptedToken): EncryptedToken {
     const tokenStr = jwt.sign(token.payload, this.secret, {
       expiresIn: this.validity,
@@ -20,6 +24,12 @@ export default class JwtTokenGenerator implements TokenGenerator {
     return new EncryptedToken(tokenStr);
   }
 
+  /**
+   * Decrypts an EncryptedToken using the verify function of jwt.
+   * @param {EncryptedToken} token a an EncryptedToken to decrypt
+   * @return {DecryptedToken | undefined} the newly decrypted token,
+   * undefined if something went wrong while decrypting
+   */
   decode(token: EncryptedToken): DecryptedToken | undefined {
     try {
       const decoded = jwt.verify(token.value, this.secret);
