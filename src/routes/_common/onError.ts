46a8b8bba09ad9ef2073281dbf0b6a972d5c5598
Tale152 <alessandro.talmi@studio.unibo.it>
2022-07-18 12:16:11 +0200

    commented onError

--- src/routes/_common/onError.ts
+++ src/routes/_common/onError.ts
@@ -1,5 +1,11 @@
 import {Response} from 'express';
 
+/**
+ * Callback commonly used in routes to send 500 to the caller
+ * @param {Response} res express Response in the route invocation
+ * @return {Promise<void>} a promise that will be resolved when
+ * the response is sent
+ */
 export function onError(res: Response): () => Promise<void> {
   return async () => {
     res.status(500).send();
