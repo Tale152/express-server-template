017c31244a6f28e3872b7567918e33efba06fe85
Tale152 <alessandro.talmi@studio.unibo.it>
2022-07-18 12:33:57 +0200

    commented get.login.ts

--- src/routes/user/get.login.ts
+++ src/routes/user/get.login.ts
@@ -5,6 +5,12 @@ import {UnpersistedUser} from '../../core/entities/User';
 import UserUseCases from '../../core/use_cases/UserUseCases';
 import {onError} from '../_common/onError';
 
+/**
+ * Handler of the GET /user/login route
+ * @param {UserUseCases} userUseCases use cases for the User
+ * @return {Promise<void>} a promise that will be solved after
+ * the request is handled
+ */
 export default function userLoginHandler(
   userUseCases: UserUseCases,
 ): (req: Request, res: Response) => Promise<void> {
@@ -29,12 +35,25 @@ export default function userLoginHandler(
   };
 }
 
+/**
+ * Handler called when the provided cretentials are not valid for performing
+ * login with any User.
+ * @param {Express} res Express response
+ * @return {function(): Promise<void>} the actual callback that will be
+ * invoked
+ */
 function onInvalidCredentials(res: Response): () => Promise<void> {
   return async () => {
     res.status(401).send();
   };
 }
 
+/**
+ * Handler called when the login request is completed successfully.
+ * @param {Express} res Express response
+ * @return {function(): Promise<void>} the actual callback that will be
+ * invoked
+ */
 function onSuccess(res: Response): (token: EncryptedToken) => Promise<void> {
   return async (token: EncryptedToken) => {
     res.status(200).json({token: token.value}).send();
