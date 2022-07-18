5407995a0fb689f91f7ec6ee1ba82f802d68f72e
Tale152 <alessandro.talmi@studio.unibo.it>
2022-07-18 16:06:42 +0200

    commented UserUseCases

--- src/core/use_cases/UserUseCases.ts
+++ src/core/use_cases/UserUseCases.ts
@@ -7,7 +7,20 @@ import EncryptionHandler from '../interface_adapters/security/EncryptionHandler'
 import UserPersistence from '../interface_adapters/persistence/UserPersistence';
 import TokenGenerator from '../interface_adapters/security/TokenGenerator';
 
+/**
+ * Use cases revolving around an User.
+ */
 export default class UserUseCases {
+  /**
+   * @param {UserPersistence} persistence concrete implementation of
+   * UserPersistence which handles the persistence and retrieval of
+   * Users
+   * @param {TokenGenerator} tokenGenerator concrete implementation of
+   * TokenGenerator, handling the generation and decryption of Tokens
+   * for authenticating the User while accessing some routes.
+   * @param {EncryptionHandler} encryptionHandler concrete implementation
+   * of EncryptionHandler, handling the encryption of passwords
+   */
   constructor(
     private persistence: UserPersistence,
     private tokenGenerator: TokenGenerator,
@@ -16,6 +29,16 @@ export default class UserUseCases {
     // does nothing
   }
 
+  /**
+   * Use case for registering an User.
+   * @param {UnpersistedUser} user to persist
+   * @param {function(): void} onUserAlreadyExists callback invoked when
+   * the provided User already exists
+   * @param {function(EncryptedToken): void} onSuccess callback invoked
+   * when the registration is completed with success
+   * @param {function(): void} onError callback invoked when an error
+   * arises while trying to satisfy the request
+   */
   register(
     user: UnpersistedUser,
     onUserAlreadyExists: () => void,
@@ -44,6 +67,17 @@ export default class UserUseCases {
     }, onError);
   }
 
+  /**
+   * Use case for the login of an User.
+   * @param {UnpersistedUser} user the User that wants to login
+   * @param {function():void} onInvalidCredentials callback invoked
+   * when the credentials contained in the provided User are not
+   * valid to perform a login to any persisted User
+   * @param {function(EncryptedToken): void} onSuccess callback
+   * invoked when the login attempt is successful
+   * @param {function():void} onError callback invoked when an error
+   * arises duing the login attempt
+   */
   login(
     user: UnpersistedUser,
     onInvalidCredentials: () => void,
@@ -71,6 +105,16 @@ export default class UserUseCases {
     }, onError);
   }
 
+  /**
+   * Use case for retieving a persisted User by providing its ID.
+   * @param {string} id the id of the User to find
+   * @param {function(User):void} onFound callback invoked when the
+   * User is successfully found
+   * @param {function(): void} onNotFound callback invoked when no
+   * User is found using the provided ID
+   * @param {function(): void} onError callback invoked when an error
+   * arises during the research process
+   */
   getById(
     id: string,
     onFound: (user: User) => void,
@@ -82,6 +126,16 @@ export default class UserUseCases {
     }, onError);
   }
 
+  /**
+   * Use case for retieving a persisted User by providing its username.
+   * @param {string} username the username of the User to find
+   * @param {function(User):void} onFound callback invoked when the
+   * User is successfully found
+   * @param {function(): void} onNotFound callback invoked when no
+   * User is found using the provided ID
+   * @param {function(): void} onError callback invoked when an error
+   * arises during the research process
+   */
   getByUsername(
     username: string,
     onFound: (user: User) => void,
@@ -94,6 +148,16 @@ export default class UserUseCases {
   }
 }
 
+/**
+ * Utility function that takes an UnpersistedUser and encrypt
+ * its password by using the EncryptionHandler.
+ * @param {UnpersistedUser} user the User which password will
+ * be encrypted
+ * @param {EncryptionHandler} encryptionHandler the concrete
+ * instance of an EncryptionHandler to perform the encryption
+ * @return {Promise<UnpersistedUser>} a promise wich, once solved,
+ * contains the new UnpersistedUser with its password encrypted
+ */
 async function encryptUser(
   user: UnpersistedUser,
   encryptionHandler: EncryptionHandler,
