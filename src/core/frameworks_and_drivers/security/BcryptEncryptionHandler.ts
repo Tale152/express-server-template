7e4b55b226d498b0b41a7aec8dee41df42b57080
Tale152 <alessandro.talmi@studio.unibo.it>
2022-07-18 16:21:30 +0200

    commented BcryptEncryptionHandler

--- src/core/frameworks_and_drivers/security/BcryptEncryptionHandler.ts
+++ src/core/frameworks_and_drivers/security/BcryptEncryptionHandler.ts
@@ -2,17 +2,33 @@ import * as bcrypt from 'bcrypt';
 import EnvVariablesSingleton from '../../../setup/EnvVariablesSingleton';
 import EncryptionHandler from '../../interface_adapters/security/EncryptionHandler';
 
+/**
+ * Concrete implementation of the EncryptionHandler inferface using the
+ * bcrypt library.
+ */
 export default class BcryptEncryptionHandler implements EncryptionHandler {
-  private salt: number;
+  private salt: number = EnvVariablesSingleton.instance.encryptionSalt;
 
-  constructor() {
-    this.salt = EnvVariablesSingleton.instance.encryptionSalt;
-  }
-
+  /**
+   * Encrypts a string using the hash function provided by bcrypt.
+   * @param {string} str a string to encrypt
+   * @return {Promise<string>} a promise wich, once solved, returns the
+   * encrypted string
+   */
   encrypt(str: string): Promise<string> {
     return bcrypt.hash(str, this.salt);
   }
 
+  /**
+   * Comares the actual data end the encrypted payload using the compare
+   * function provided by bcrypt.
+   * @param {string} data the correct data to compare to the encrypted
+   * payload
+   * @param {string} encrypted the encrypted payload to compare to the
+   * correct data
+   * @return {Promise<boolean>} a promise wich, once solved, returns
+   * true if the comparison is successfull, false otherwise
+   */
   compare(data: string, encrypted: string): Promise<boolean> {
     return bcrypt.compare(data, encrypted);
   }
