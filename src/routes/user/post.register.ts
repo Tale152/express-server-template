26eef8b978a14fcde3e57b1c095fae1e3226e52c
Tale152 <alessandro.talmi@studio.unibo.it>
2022-07-18 12:27:05 +0200

    commented post.register.ts

--- src/routes/user/post.register.ts
+++ src/routes/user/post.register.ts
@@ -6,6 +6,12 @@ import UserUseCases from '../../core/use_cases/UserUseCases';
 import {isStringEmpty} from '../../core/utils/checks/stringChecks';
 import {onError} from '../_common/onError';
 
+/**
+ * Handler of the POST /user/regist route
+ * @param {UserUseCases} userUseCases use cases for the User
+ * @return {Promise<void>} a promise that will be solved after
+ * the request is handled
+ */
 export default function userRegisterHandler(
   userUseCases: UserUseCases,
 ): (req: Request, res: Response) => Promise<void> {
@@ -25,16 +31,35 @@ export default function userRegisterHandler(
   };
 }
 
+/**
+ * Checks if the parameters for the request are valid.
+ * @param {string} username the provided username
+ * @param {string} password the provided password
+ * @return {boolean} true if the parameters are valid, false otherwise
+ */
 function areParametersValid(username: string, password: string): boolean {
   return !isStringEmpty(username) && !isStringEmpty(password);
 }
 
+/**
+ * Handler called when the request cannot be completed since the provided
+ * User already exists.
+ * @param {Express} res Express response
+ * @return {function(): Promise<void>} the actual callback that will be
+ * invoked
+ */
 function onUserAlreadyExists(res: Response): () => Promise<void> {
   return async () => {
     res.status(406).send();
   };
 }
 
+/**
+ * Handler called when the registration request is completed successfully.
+ * @param {Express} res Express response
+ * @return {function(): Promise<void>} the actual callback that will be
+ * invoked
+ */
 function onSuccess(res: Response): (token: EncryptedToken) => Promise<void> {
   return async (token: EncryptedToken) => {
     res.status(201).json({token: token.value}).send();
