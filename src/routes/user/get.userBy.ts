d5bd07af61ae1e1f8b6a76612feaddb1029bcc1a
Tale152 <alessandro.talmi@studio.unibo.it>
2022-07-18 12:31:03 +0200

    commented get.userBy.ts

--- src/routes/user/get.userBy.ts
+++ src/routes/user/get.userBy.ts
@@ -4,6 +4,12 @@ import User from '../../core/entities/User';
 import UserUseCases from '../../core/use_cases/UserUseCases';
 import {onError} from '../_common/onError';
 
+/**
+ * Handler of the GET /user/get-by route
+ * @param {UserUseCases} userUseCases use cases for the User
+ * @return {Promise<void>} a promise that will be solved after
+ * the request is handled
+ */
 export default function userGetByHandler(
   userUseCases: UserUseCases,
 ): (req: Request, res: Response) => Promise<void> {
@@ -30,6 +36,12 @@ export default function userGetByHandler(
   };
 }
 
+/**
+ * Handler called when an User is found using the provided parameters.
+ * @param {Express} res Express response
+ * @return {function(User): Promise<void>} the actual callback that will be
+ * invoked, containing the retreived User
+ */
 function onFound(res: Response): (user: User) => Promise<void> {
   return async (user: User) => {
     res
@@ -42,6 +54,12 @@ function onFound(res: Response): (user: User) => Promise<void> {
   };
 }
 
+/**
+ * Handler called when no User was found using the provided parameters.
+ * @param {Express} res Express response
+ * @return {function(): Promise<void>} the actual callback that will be
+ * invoked
+ */
 function onNotFound(res: Response): () => Promise<void> {
   return async () => {
     res.status(204).send();
